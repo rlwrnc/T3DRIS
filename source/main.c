42a8be0e9d171ac68a357ecfc25fa7b1935a2c29
rlwrnc <raymond.lawrence@tutanota.com>
2023-07-29 18:03:13 -0400

    initial commit

---
+++ source/main.c
@@ -0,0 +1,31 @@
+#include "memory.h"
+
+#include <stdbool.h>
+#include "glfw/glfw3.h"
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+	glfwInit();
+	GLFWwindow *window = glfwCreateWindow(1920, 1080, "t3dris", 0, 0);
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+
+	GameMemory game_memory = memory_game_allocate(kilobytes(64), megabytes(512));
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	memory_game_deallocate(&game_memory);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
